a20ed6aca2b99d9496782acffe5559b3f2bff116
Vishal Shah <visushah99@gmail.com>
2022-05-02 01:31:48 -0400

    Updated with Synthetic Data

--- src/get_data.py
+++ src/get_data.py
@@ -1,13 +1,27 @@
 import pandas as pd
 import streamlit as st
+from sklearn.datasets import make_blobs
+from numpy import cov
+from scipy.stats import pearsonr
 
 def dataframe(source):
     if source=="US Census Data":
         data = pd.read_csv("data/census.csv", na_values=' ?')
         return data
     else:
-        return 
-
-
-def synthetic():
-    pass
+        n_samples=5000
+        n_features=3
+        random_state=1
+        return_centers=True
+        
+        x, y, centers = make_blobs(n_samples=n_samples, 
+                                   n_features=n_features, 
+                                   random_state=random_state, 
+                                   return_centers=return_centers)
+        
+        covariance = cov(x[:,0], x[:, 1])
+        st.write("Correlation of Features vs Target : ", pearsonr(x[:,0], x[:, 1]))
+        
+        final_df = pd.DataFrame(x,columns=['x1','y1','z1'])
+        final_df['target'] = y
+        return final_df
